4617082c393e680a0cd33153140563c32673b0f7
marwa <mds08@mail.aub.edu>
2022-08-01 21:43:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// first task
+const main = document.getElementById("main");
+main.remove();
+
+// second task
+const body= document.body;
+const newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML='Marwa is the champion';
+body.append(newHeader);
